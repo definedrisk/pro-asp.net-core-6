ee81fa37e9b7e47794350deffa29b4a3084441ce
definedrisk <definedrisk@outlook.com>
2022-06-06 10:58:08 +0100

    Ch5 part 1 (Ubuntu)
    Understanding null state.

---
+++ Ubuntu 20.04.03 LTS/05/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+// using Microsoft.AspNetCore.Mvc;
+// using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product?[] products = Product.GetProducts();
+            return View(new string[] { products[0]!.Name ?? "No Value" });
+
+        }
+    }
+}
